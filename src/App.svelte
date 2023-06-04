7e11dbcfe7ca06fdea9ce7699f97eab2ddb49912
cahillc <caracahill@outlook.com>
2023-06-04 13:28:30 +0100

    no message

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 </script>
+<meta name="viewport" content="width=device-width,initial-scale=1"/>
 <section class="has-background-black">
     <div class="title has-text-centered has-text-white"> Eoin & Eleanor's Dance Party</div>
     <div class="container">
@@ -9,7 +10,6 @@
         <li>Erin's Isle GAA Function Room</li>
         <li>2:30 - 4:30pm </li>
        </ul>
-       <br>
        <p> Eoin and Eleanor will be celebrating birthdays with a dance party. 
         Parents and siblings are more than welcome to attend. A bar is in the function room if parents want to grab a drink. Plenty of parking on premise. The 140 bus stop is a five minute walk.</p>
       </div>
