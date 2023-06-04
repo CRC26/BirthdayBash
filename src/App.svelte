f1f086c3dab7068460f78b60bcfe6d87bdd68574
cahillc <caracahill@outlook.com>
2023-06-04 19:40:57 +0100

    no message

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
        Eoin & Eleanor's Dance Party
   </div>
   <div class="columns is-full-mobile">
-      <div class="column is-half is-full-mobile has-text-centered">
+      <div class="column is-half-mobile has-text-centered">
         <ul>
           <li>Date: June 25, 2023</li> 
           <li>Location: Erin's Isle GAA Function Room</li>
@@ -16,7 +16,7 @@
            A bar is in the function room if parents want to grab a drink. Plenty of parking on premise. The 140 bus stop is a five minute walk.
         </p>
       </div>
-      <div class="column is-auto is-mobile">
+      <div class="column is-auto-mobile">
         <figure class='image'>
            <img src= "/ee2.jpg" alt="children sitting beside water">
         </figure>
