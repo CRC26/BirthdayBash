dadbece2b107ffe9bdbeaa4e0a298511b201829b
cahillc <caracahill@outlook.com>
2023-06-04 15:06:37 +0100

    no message

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script>
 </script>
-<section class="is-widescreen has-background-black">
-    <div class="title is-widescreen has-text-centered has-text-white"> Eoin & Eleanor's Dance Party</div>
-    <div class="container is-widescreen">
+<section class="is-fluid has-background-black">
+    <div class="title is-fluid has-text-centered has-text-white"> Eoin & Eleanor's Dance Party</div>
+    <div class="container is-fluid">
       <div class="menu">
        <ul>
         <h1>Date: June 25, 2023</h1> 
@@ -13,7 +13,7 @@
         Parents and siblings are more than welcome to attend. A bar is in the function room if parents want to grab a drink. Plenty of parking on premise. The 140 bus stop is a five minute walk.</p>
       </div>
       </div>
-    <div class="right is-widescreen">
+    <div class="right is-fluid">
         <p>
          <img src="/ee2.jpg" class="large-right-img" style="width:50%" alt="children sitting beside water">
     </div>
