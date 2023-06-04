c27e9b60a3cdf550a6849d424fe3dc39c9977ed5
cahillc <caracahill@outlook.com>
2023-06-04 11:52:48 +0100

    Birthday Project

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+<section class="has-background-black">
+    <div class="title has-text-centered has-text-white"> Eoin & Eleanor's Dance Party</div>
+    <div class="container">
+      <div class="menu">
+       <ul>
+        <h1>June 25, 2023</h1> 
+        <li>Erin's Isle GAA Function Room</li>
+        <li>2:30 - 4:30pm </li>
+       </ul>
+       <br>
+       <p> Eoin and Eleanor will be celebrating birthdays with a dance party. 
+        Parents and siblings are more than welcome to attend. A bar is in the function room if parents want to grab a drink. Plenty of parking on premise. The 140 bus stop is a five minute walk.</p>
+      </div>
+      </div>
+    <div class="right">
+        <p>
+         <img src="/ee2.jpg" class="large-right-img"alt="Dublin Life Style Photographer.Small girl in a unicorn halloween costume surrounded by fall leaves">
+    </div>
+</section>
+
+
+      
+      
+
+
