d2a6a52fe40c9d5e84ba0dee3607b817dbcbe5f2
cahillc <caracahill@outlook.com>
2023-06-04 19:05:47 +0100

    updated

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,27 @@
 <script>
 </script>
-<section class="is-fluid has-background-black">
-    <div class="title is-fluid has-text-centered has-text-white"> Eoin & Eleanor's Dance Party</div>
-    <div class="container is-fluid">
-      <div class="menu">
-       <ul>
-        <h1>Date: June 25, 2023</h1> 
-        <li>Location: Erin's Isle GAA Function Room</li>
-        <li>Time: 2:30 - 4:30pm </li>
-       </ul>
-       <p> Eoin and Eleanor will be celebrating birthdays with a dance party. 
-        Parents and siblings are more than welcome to attend. A bar is in the function room if parents want to grab a drink. Plenty of parking on premise. The 140 bus stop is a five minute walk.</p>
+  <div class="title is-full has-text-centered">
+       Eoin & Eleanor's Dance Party
+  </div>
+  <div class="columns">
+      <div class="column is-half has-text-centered">
+        <ul>
+          <li>Date: June 25, 2023</li> 
+          <li>Location: Erin's Isle GAA Function Room</li>
+          <li>Time: 2:30 - 4:30pm </li>
+        </ul>
+        <br>
+        <p>Eoin and Eleanor will be celebrating birthdays with a dance party. 
+           Parents and siblings are more than welcome to attend.
+           A bar is in the function room if parents want to grab a drink. Plenty of parking on premise. The 140 bus stop is a five minute walk.
+        </p>
       </div>
+      <div class="column is auto">
+        <figure class='image is-250x128'>
+           <img src= "/ee2.jpg" alt="children sitting beside water">
+        </figure>
       </div>
-    <div class="right is-fluid">
-        <p>
-         <img src="/ee2.jpg" class="large-right-img" style="width:50%" alt="children sitting beside water">
-    </div>
-</section>
-
+  </div>
 
       
       
