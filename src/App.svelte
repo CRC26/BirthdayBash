4432d8b013fbae105fe4198c966d4a12b747e4ca
cahillc <caracahill@outlook.com>
2023-06-04 15:02:43 +0100

    Updated response.

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,21 @@
 <script>
 </script>
-<meta name="viewport" content="width=device-width,initial-scale=1"/>
-<section class="has-background-black">
-    <div class="title has-text-centered has-text-white"> Eoin & Eleanor's Dance Party</div>
-    <div class="container">
+<section class="is-widescreen has-background-black">
+    <div class="title is-widescreen has-text-centered has-text-white"> Eoin & Eleanor's Dance Party</div>
+    <div class="container is-widescreen">
       <div class="menu">
        <ul>
-        <h1>June 25, 2023</h1> 
-        <li>Erin's Isle GAA Function Room</li>
-        <li>2:30 - 4:30pm </li>
+        <h1>Date: June 25, 2023</h1> 
+        <li>Location: Erin's Isle GAA Function Room</li>
+        <li>Time: 2:30 - 4:30pm </li>
        </ul>
        <p> Eoin and Eleanor will be celebrating birthdays with a dance party. 
         Parents and siblings are more than welcome to attend. A bar is in the function room if parents want to grab a drink. Plenty of parking on premise. The 140 bus stop is a five minute walk.</p>
       </div>
       </div>
-    <div class="right">
+    <div class="right is-widescreen">
         <p>
-         <img src="/ee2.jpg" class="large-right-img"alt="Dublin Life Style Photographer.Small girl in a unicorn halloween costume surrounded by fall leaves">
+         <img src="/ee2.jpg" class="large-right-img" style="width:50%" alt="children sitting beside water">
     </div>
 </section>
 
